514a7f3df1a7bf75aa32643ca4cec936885712a8
João Sucupira <jasucupira@gmail.com>
2024-07-15 09:17:09 -0300

    Finished with basic classes. In doubt on how to use ostringstream type.

--- Prova2022.1/Diagnosticador.cpp
+++ Prova2022.1/Diagnosticador.cpp
@@ -41,7 +41,9 @@ class Endorfina : public Hormonio {
         float real;
         Serotonina* pSero;
     public:
-        Endorfina() : Hormonio() {}
+        Endorfina() : Hormonio() {
+            real = 1.0 * (rand() % 11); 
+        }
         ~Endorfina() {
             pSero = NULL;
         } 
@@ -53,7 +55,71 @@ class Endorfina : public Hormonio {
 };
 
 // Classe derivada de: Hormonio (polimorfica:calcular_nivel)
-class Serotonina {};
+class Serotonina : public Hormonio {
+    private:
+        int inteiro;
+        Endorfina* pEndo;
+    public:
+        Serotonina() : Hormonio() {
+            inteiro = rand() % 11;
+            if (estresse == true && inteiro > 0) { inteiro--; }
+        }
+        ~Serotonina() {
+            pEndo = NULL;
+        }
+        void setEndo(Endorfina* pE) {
+            pEndo = pE;
+        }
+        void calcular_nivel(); // Alhures para evitar dependencia
+        const int getInteiro() const { return inteiro; }
+};
+
+// Polimorficos:
+void Endorfina::calcular_nivel() {
+    int intSero = pSero->getInteiro();
+    if (real >= 6.0 && real <= 10.0 && intSero > 7) {
+        nivel = "normal";
+    } else if (real > 3.0 && real < 6.0 && intSero > 5) {
+        nivel = "medio";
+    } else if (real >= 0.0 && real <= 3.0 && intSero > 0) {
+        nivel = "baixo";
+    } 
+}
+
+void Serotonina::calcular_nivel() {
+    float realEndo = pEndo->getReal();
+    if (inteiro >= 7 && inteiro <= 10 && realEndo > 5.0) {
+        nivel = "normal";
+    } else if (inteiro > 4 && inteiro < 7 && realEndo > 3.0) {
+        nivel = "medio";
+    } else if (inteiro >= 0.0 && inteiro <= 4.0 && realEndo > 0.0) {
+        nivel = "baixo";
+    } 
+}
+
+// Classe gerenciadora que agrega hormonios do paciente
+// Agrega: 1 SEROTONINA, 1 ENDORFINA
+class Reg_Paciente {
+    private:
+        bool ok;
+        Endorfina nivel_endo;
+        Serotonina nivel_sero;
+    public: 
+        Reg_Paciente() {
+            nivel_endo.calcular_nivel();
+            nivel_sero.calcular_nivel();
+        }
+        const int getOK() const { return ok; }
+        void auto_avaliar() {
+            if (nivel_endo.getNivel() == "baixo" || nivel_sero.getNivel() == "baixo") {
+                ok = false;
+            } else {
+                ok = true;
+            }
+        }
+};
+
+// Classe principal executadora das classes do projeto
 class Diagnosticador {
     private:
         list<Reg_Paciente*> listap;
@@ -69,7 +135,10 @@ class Diagnosticador {
 
 };
 
+
 int main() {
+    srand(time(NULL));
+
     Diagnosticador diag1(100);
     // cout << diag1 << endl;
     return 0;
